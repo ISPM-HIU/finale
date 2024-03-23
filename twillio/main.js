d7df35758b6ea81ee8cb7eaaa8cf675c4bf7af1e
RamamonjizafyAndo <ramamonjizafymanitra06@gmail.com>
2024-03-23 23:23:02 +0300

    fix

--- twillio/main.js
+++ twillio/main.js
@@ -3,10 +3,6 @@ const bodyParser = require('body-parser');
 const { MessagingResponse } = require('twilio').twiml;
 const axios = require('axios');
 
-const accountSid = 'AC20958dc1c0bd4bbb96eb871337634b52';
-const authToken = '73e8e21c157748c4ebae61be2fc8a2d2';
-const client = require('twilio')(accountSid, authToken);
-
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
